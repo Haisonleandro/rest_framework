f2ac46092a55d4b88c1c5cc3cf4c150b5cbb797d
Haisonleandro <haisonleandrotoro@gmail.com>
2024-09-13 10:13:52 -0500

    creacion de carpeta apps y un model

---
+++ Documents/adso/phyton viernes/rest_framework/restframework/apps/rest/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class rest(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    orden = models.TextField()
+    crear = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
